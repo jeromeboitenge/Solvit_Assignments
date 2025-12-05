cc63fa74de74a2fa5fd41365136dc5418cd4f0e4
boitenge <jeromeboitenge@gmail.com>
2025-12-05 14:34:21 +0200

    hello

--- managing_students/typescript/src/index.ts
+++ managing_students/typescript/src/index.ts
@@ -1,3 +1,7 @@
-
-const myName: string = 'jerome'
-console.log(myName);
+let sales: number = 123456789;
+let course = 'Typescript'
+let is_Published = true;
+let level;
+function render(document: any) {
+    console.log(document)
+}
