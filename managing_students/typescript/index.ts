3fd4f6e84a2a46d4d685aeb32a6621c096717274
boitenge <jeromeboitenge@gmail.com>
2025-12-05 12:16:12 +0200

    s

--- managing_students/typescript/index.ts
+++ managing_students/typescript/index.ts
@@ -6,21 +6,21 @@
 // var myNames:string// use varn wherever you are not going to initialise directly
 // myNames='d'
 // console.log(typeof isActive)
-enum Gender {
-    MALE = 'Male',
-    FEMALE = 'Female',
-    OTHER = 'Other'
-}
+// enum Gender {
+//     MALE = 'Male',
+//     FEMALE = 'Female',
+//     OTHER = 'Other'
+// }
 
-type Student = {
-    name: string
-    age: number
-    isActive?: boolean
-    isAvailable: "yes" | "no"
-}
-const maxi: Student = {
-    name: 'maxi',
-    age: 34,
-    isAvailable: 'no'
-}
-interface User<y>
+// type Student = {
+//     name: string
+//     age: number
+//     isActive?: boolean
+//     isAvailable: "yes" | "no"
+// }
+// const maxi: Student = {
+//     name: 'maxi',
+//     age: 34,
+//     isAvailable: 'no'
+// }
+// interface User<y>
