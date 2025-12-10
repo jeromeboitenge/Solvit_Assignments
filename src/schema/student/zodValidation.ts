b1777e937ed3dbe7793d2a1630f25cee2389649a
boitenge <jeromeboitenge@gmail.com>
2025-12-10 14:52:43 +0200

    adding validations

--- src/schema/student/zodValidation.ts
+++ src/schema/student/zodValidation.ts
@@ -1,6 +1,9 @@
 import zod from "zod"
 import { studentValidationInterface } from "../../types/student.interface"
 export const createStudentValidationSchema = zod.object({
-
-
+    name: zod.string().min(3),
+    age: zod.number().min(18).max(65),
+    isActive: zod.boolean()
 })
+
+
