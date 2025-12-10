c138eb6face753ac5004ecc604e6b9516080652f
boitenge <jeromeboitenge@gmail.com>
2025-12-10 09:35:17 +0200

    adding response

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,22 +1,37 @@
 import { Response } from "express";
+interface ResponseInterface<T> {
+    res: Response;
+    data?: T;
+    message?: string;
+    statusCode?: number;
+    success?: boolean;
+    error?: any;
+}
 
+export class ResponseService {
+    response = <T>({
+        res,
+        data,
+        statusCode = 200,
+        success = true,
+        message = "Fetched well",
+        error,
+    }: ResponseInterface<T>): Response => {
+        if (statusCode > 200 && statusCode < 299) {
+            success = true;
+            error = undefined;
+        }
+        if (statusCode > 300 && statusCode < 599) {
+            success = false;
+            error = data;
+        }
 
-// interface ResponseInterface<T, Z> {
-//     statusCode: number
-//     sucess: boolean;
-//     message: string;
-//     data?: T;
-//     error?: Z;
-// }
-
-
-
-// Response = <T>({
-//     res,
-//     data,
-//     statusCode=200,
-//     success="fetched well",
-//     error,
-
-
-// }) 
+        return res.status(statusCode).json({
+            data,
+            success,
+            statusCode,
+            message,
+            error,
+        });
+    };
+}
