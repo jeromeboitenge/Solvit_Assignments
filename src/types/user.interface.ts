bb91d5e3c6281dfb644ffa489da319f97fe61021
boitenge <jeromeboitenge@gmail.com>
2025-12-10 16:53:13 +0200

    s

---
+++ src/types/user.interface.ts
@@ -0,0 +1,10 @@
+export interface userInterface {
+    "name": string,
+    "email": string
+    "phoneNumber": string
+    "gender": Gender
+}
+enum Gender {
+    Male = "Male",
+    Female = "Female"
+}
