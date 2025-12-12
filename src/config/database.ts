ab17d0744cc9fcca69f6fa3ace63220c88739d5e
boitenge <jeromeboitenge@gmail.com>
2025-12-12 14:43:20 +0200

    d

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,10 +4,11 @@ dotenv.config();
 export const databaseConnection = async () => {
     var db_url: NonNullable<string> = process.env.DATABASE_URL as string
     const db_username: NonNullable<string> = process.env.DATABASE_USERNAME as string
-    const db_password: NonNullable<string> = process.env.DATABASE_PASSWORD as string
+    const db_password: NonNullable<string> = process.env.PASSWORD as string
+
 
     try {
-        db_url = db_url.replace("<db_userbame>", db_username).replace("<db_password>", db_password);
+        db_url = db_url.replace("<db_username>", db_username).replace("<db_password>", db_password);
         await mongoose.connect(db_url, {}).then((value) => {
             console.log('Our Database is Running ')
 
