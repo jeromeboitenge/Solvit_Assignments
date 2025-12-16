43bae4adac5bae078874ff196cf49156b88e2d25
boitenge <jeromeboitenge@gmail.com>
2025-12-16 09:46:30 +0200

    adding login interace

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,1 +1,9 @@
-const
+import { AuthService } from "../services/auth.service";
+import { UsersServices } from "../services/users.service";
+import { ResponseService } from "../utils";
+
+const responseService = new ResponseService()
+const authService = new AuthService()
+const userService = new UsersServices
+
+
