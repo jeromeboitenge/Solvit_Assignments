1cca4abdd55d480d15099efebe6f8ebfd2912858
boitenge <jeromeboitenge@gmail.com>
2025-12-16 09:23:49 +0200

    configurind security on passwords and generating tokens

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,6 +1,32 @@
 import * as bcrypt from "bcryptjs"
+import { config } from "../config";
+import * as jwt from "jsonwebtoken";
 
 export const hashedPassword = (password: string): string => {
     return bcrypt.hashSync(password, 12)
+
+};
+export interface generateTokenPayload {
+    id: string,
+    role: string
+}
+export const comparePassword = async (
+    password: string, db_password: string): Promise<boolean> => {
+    return await bcrypt.compare(password, db_password)
+}
+
+export const generateToken = async ({ id, role }: generateTokenPayload) => {
+    return jwt.sign(
+        {
+            sub: id,
+            role,
+        },
+        config.jwtSecret,
+        { expiresIn: "1hr" }
+    );
+};
+
+export const validateToken = async (token: string) => {
+    return
 
 };
